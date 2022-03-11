4a4dc030628436632c6b30fbcde17710e4d44fd3
Rola-Ahmed <75480678+Rola-Ahmed@users.noreply.github.com>
2022-03-11 23:56:23 +0200

    ...

--- Test Test/Test Test/lib/pages/calender.dart
+++ Test Test/Test Test/lib/pages/calender.dart
@@ -1,68 +1,92 @@
 import 'package:flutter/material.dart';
-import 'package:nepali_utils/nepali_utils.dart';
 
-import 'package:busgo/modes/calendar_date_picker_widget.dart';
-import 'package:busgo/modes/calendar_date_range_picker_widget.dart';
-import 'package:busgo/modes/date_picker_widget.dart';
-import 'package:busgo/modes/date_range_picker_widget.dart';
+//import 'package:table_calendar/table_calendar.dart';
+import 'package:busgo/table_calendar.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+import '../calender_pages/basics_example.dart';
+import '../calender_pages/complex_example.dart';
+import '../calender_pages/events_example.dart';
+
+import '../calender_pages/multi_example.dart';
+import '../calender_pages/range_example.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       theme: ThemeData(
+//         primarySwatch: Colors.green,
+//       ),
+//       home: HomeCalendarPage(),
+//     );
+//   }
+// }
 
 class Calender extends StatefulWidget {
   @override
-  State<Calender> createState() => CalenderState();
-  //State<FromTo> createState() => MapFromToState();
-} 
+  _CalenderState createState() => _CalenderState();
+}
 
-class CalenderState extends State<Calender> {
+class _CalenderState extends State<Calender> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Calendar Example'),
       ),
-      title: 'Nepali Date Picker Demo',
-      home: DefaultTabController(
-        length: 4,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("Nepali Date Picker"),
-            centerTitle: true,
-            bottom: TabBar(
-              isScrollable: true,
-              tabs: [
-                Tab(text: 'Date Picker'),
-                Tab(text: 'Calendar'),
-                Tab(text: 'Date Range Picker'),
-                Tab(text: 'Calendar Range'),
-              ],
-            ),
-            actions: [
-              IconButton(
-                icon: Text(
-                  NepaliUtils().language == Language.english ? 'ने' : 'En',
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline6
-                      ?.copyWith(color: Colors.white),
-                ),
-                onPressed: () {
-                  NepaliUtils().language =
-                      NepaliUtils().language == Language.english
-                          ? Language.nepali
-                          : Language.english;
-                  setState(() {});
-                },
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              child: Text('Basics'),
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => TableBasicsExample()),
               ),
-            ],
-          ),
-          body: TabBarView(
-            children: [
-              DatePickerWidget(),
-              CalendarDatePickerWidget(),
-              DateRangePickerWidget(),
-              CalendarDateRangePickerWidget(),
-            ],
-          ),
+            ),
+            // const SizedBox(height: 12.0),
+            // ElevatedButton(
+            //   child: Text('Range Selection'),
+            //   onPressed: () => Navigator.push(
+            //     context,
+            //     MaterialPageRoute(builder: (_) => TableRangeExample(),
+            //     ),
+            //   ),
+            // ),
+            const SizedBox(height: 12.0),
+            ElevatedButton(
+              child: Text('Events'),
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => TableEventsExample()),
+              ),
+            ),
+            // const SizedBox(height: 12.0),
+            // ElevatedButton(
+            //   child: Text('Multiple Selection'),
+            //   onPressed: () => Navigator.push(
+            //     context,
+            //     MaterialPageRoute(
+            //       builder: (_) => TableMultiExample(),
+            //     ),
+            //   ),
+            // ),
+            const SizedBox(height: 12.0),
+            ElevatedButton(
+              child: Text('Complex'),
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => TableComplexExample()),
+              ),
+            ),
+            const SizedBox(height: 20.0),
+          ],
         ),
       ),
     );
